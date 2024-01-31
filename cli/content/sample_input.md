327dee1f10ca08ba0c31d2e7d5a892381b1f76f7
404salad <404salad@gmail.com>
2024-01-31 23:52:42 +0530

    created cli

--- cli/content/sample_input.md
+++ cli/content/sample_input.md
@@ -1,6 +1,6 @@
 If you don't use GDB but program in C/C++, you are missing out on a powerful debugging tool. Here's a quick 5-minute tutorial to get you started.
 
-# mai kisi ka sapne hun1
+# mai kisi ka sapne hun1 jo aaj ban chuka hun jhooth
 
 ## Step 1: Compile with Debug Symbols
 
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,21 @@
 pub mod parse;
+use std::thread;
+use std::time::Duration;
 
-fn main() {
+use console::Term;
+
+fn main(){
+    let term = Term::stdout();
+    term.clear_screen();
+    term.write_line("
+    ██████   ██████  ███    ██ ██████      ███████ ███████  ██████  
+    ██   ██ ██    ██ ████   ██ ██   ██     ██      ██      ██       
+    ██████  ██    ██ ██ ██  ██ ██   ██     ███████ ███████ ██   ███ 
+    ██      ██    ██ ██  ██ ██ ██   ██          ██      ██ ██    ██ 
+    ██       ██████  ██   ████ ██████      ███████ ███████  ██████  
+                    ");
+    thread::sleep(Duration::from_millis(200));
+    term.write_line("A simple cli tool to convert markdown to blog");
     let _ = parse::markdown_to_styled_html("sample_input");
     /*
     let path = std::env::args()
