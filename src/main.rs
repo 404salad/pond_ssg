0c19271c633b8237b783159a96b01e112951cd53
404salad <404salad@gmail.com>
2024-09-05 18:41:33 +0530

    finished setting up rayon

--- src/main.rs
+++ src/main.rs
@@ -51,14 +51,12 @@ fn main() {
             }
         }
     });
-    /*
-        match consolidate_into_homepage::create_homepage(&user_config) {
-            Ok(_) => {
-                println!("succesfully created homepage")
-            }
-            Err(e) => {
-                eprintln!("unsuccesful in creating homepage {}", e)
-            }
-        };
-    */
+    match consolidate_into_homepage::create_homepage(&user_config) {
+        Ok(_) => {
+            println!("succesfully created homepage")
+        }
+        Err(e) => {
+            eprintln!("unsuccesful in creating homepage {}", e)
+        }
+    };
 }
